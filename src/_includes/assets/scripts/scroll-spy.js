1b525f1315d480352416d74badfa5d3b087aa1b1
Kyle Mitofsky <KyleMit@gmail.com>
2019-09-05 00:23:56 -0400

    fix smooth scroll and mobile toc layout

--- src/_includes/assets/scripts/scroll-spy.js
+++ src/_includes/assets/scripts/scroll-spy.js
@@ -5,10 +5,12 @@ var spy = new Gumshoe('.toc a');
 if (document.body.scrollIntoView) {
     document.body.addEventListener('click',function(e) {
         if (e.target.tagName === "A" && e.target.attributes.href.value.slice(0,1)) {
-            e.preventDefault();
             var id = e.target.attributes.href.value.slice(1)
             var el = document.getElementById(id)
-            el.scrollIntoView({behavior: "smooth"})
+            if (el) {
+                e.preventDefault();
+                el.scrollIntoView({behavior: "smooth"})
+            }
         }
     })
 }
