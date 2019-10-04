f307ecfc845a29d104a5cccac06bb90f51911c09
Kyle Mitofsky <KyleMit@gmail.com>
2019-10-04 13:35:27 -0500

    rename settings scripts

--- assets/scripts/settings-display-density.js
+++ assets/scripts/settings-display-density.js
@@ -1,22 +1,22 @@
 // on form load, set radio and body (immediately after body opening tag)
-function setDisplay(val) {
+function setSiteTheme(val) {
     var displayCard = val === "card"
     document.body.classList.toggle("display-card", displayCard)
     document.body.classList.toggle("display-list", !displayCard)
 }
 
 // get stored value or default
-var displayVal = localStorage.getItem('display-toggle') || "card"
+var siteThemeVal = localStorage.getItem('display-toggle') || "card"
 
 // immediately set style to prevent FOUC
-setDisplay(displayVal)
+setSiteTheme(siteThemeVal)
 
 
 // wait for the dom load to parse remaining elements
 document.addEventListener("DOMContentLoaded", function(){
 
     // set appropriate state on checkboxes once they've loaded
-    var displayEl = document.querySelector("input[type='radio'][name='display-toggle'][value='" + displayVal + "']")
+    var displayEl = document.querySelector("input[type='radio'][name='display-toggle'][value='" + siteThemeVal + "']")
     if (displayEl) { displayEl.checked = true}
 
 
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function(){
 	document.querySelectorAll("input[type='radio'][name='display-toggle']").forEach(function(el) {
         el.addEventListener("change",function(e) {
             var curDisplayVal = e.target.value
-            setDisplay(curDisplayVal)
+            setSiteTheme(curDisplayVal)
             localStorage.setItem('display-toggle', curDisplayVal)
         });
     });
