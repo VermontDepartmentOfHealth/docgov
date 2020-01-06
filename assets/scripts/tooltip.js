5adcb946c359876f6ebe60988694ba3ec3ecf74a
KyleMit <KyleMit@gmail.com>
2020-01-06 12:35:15 -0500

    #85 remove tooltips from #23

--- assets/scripts/tooltip.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll("body [title]").forEach(function(el) {
-    el.setAttribute('data-tooltip', el.title)
-    el.setAttribute('aria-label', el.title)
-    el.setAttribute('tabindex', 0)
-    el.classList.add("tooltip")
-    el.title = ""
-  });
