afa8efc230206b2504109dfe27cfbab0c9386c11
Mitofsky <Kyle.Mitofsky@vermont.gov>
2020-07-16 15:24:28 -0400

    update slide pagination

--- assets/scripts/slide.js
+++ assets/scripts/slide.js
@@ -1,10 +1,10 @@
 let NavigatePrev = function() {
     let prev = document.querySelector(".pagination-item.prev")
-    if (prev) {prev.click()}
+    if (prev) { prev.click() }
 }
 let NavigateNext = function() {
     let next = document.querySelector(".pagination-item.next")
-    if (next) {next.click()}
+    if (next) { next.click() }
 }
 
 // can't attach to body (it will be removed)
@@ -15,14 +15,3 @@ document.addEventListener("keydown", function(event) {
         NavigateNext()
     }
 })
-
-if (document.querySelector(".typed-text")) {
-    var options = {
-        strings: ['Fast', 'Awesome', 'Secure', 'Simple', 'Serverless', 'Amazing', 'Deployable'],
-        typeSpeed: 90,
-        backDelay: 1400,
-        loop: true
-    };
-    
-    var typed = new Typed('.typed-text', options);
-}
